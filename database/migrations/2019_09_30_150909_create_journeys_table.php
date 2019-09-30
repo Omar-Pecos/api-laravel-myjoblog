0304dc8905a6834900c7f6ad056c03e963148cc3
omarpecos <omarpecos@gmail.com>
2019-09-30 17:24:14 +0200

    Primer commit

---
+++ database/migrations/2019_09_30_150909_create_journeys_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJourneysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journeys', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->date('date');
+            $table->date_time_set('initial_time');
+            $table->date_time_set('final_time');
+            $table->string('initial_pos');
+            $table->string('final_pos');
+            $table->float('time');
+            $table->string('signature');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journeys');
+    }
+}
